da00f685677881bd2083dd6e2b2323680bdb24ec
phquartin <phquartinmatos@sempreceub.com>
2025-03-08 15:25:35 -0300

    feature: Aulas Classes Ultilitarias - StringBuilder

---
+++ src/phquartin/maratonajava/javacore/Qstring/test/StringBuilderTest01.java
@@ -0,0 +1,10 @@
+package phquartin.maratonajava.javacore.Qstring.test;
+
+public class StringBuilderTest01 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Pedro");
+        sb.append(" Maratona Kail");
+        System.out.println(sb);
+
+    }
+}
