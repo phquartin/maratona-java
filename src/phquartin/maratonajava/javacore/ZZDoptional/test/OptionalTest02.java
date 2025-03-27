1aa56d11e02607010799967752cb965f6d458850
phquartin <phquartinmatos@sempreceub.com>
2025-03-27 15:40:53 -0300

    feature: Aulas Optional

--- src/phquartin/maratonajava/javacore/ZZDoptional/test/OptionalTest02.java
+++ src/phquartin/maratonajava/javacore/ZZDoptional/test/OptionalTest02.java
@@ -7,8 +7,7 @@ import java.util.Optional;
 
 public class OptionalTest02 {
     public static void main(String[] args) {
-        Optional<Manga> manga = MangaRepository.findById(10);
-        Manga novoManga = manga.orElse(new Manga("Novo manga", 20));
-        System.out.println(novoManga);
+        Optional<Manga> manga = Optional.of(MangaRepository.findById(1).orElse(new Manga("Novo manga", 20)));
+        System.out.println(manga);
     }
 }
