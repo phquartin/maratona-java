be10a33fa10f9c458a3e7194ab98564410cae476
phquartin <phquartinmatos@sempreceub.com>
2025-03-31 14:36:37 -0300

    feature: Aulas Threads - Classes ThreadSafe

---
+++ src/phquartin/maratonajava/javacore/ZZFthreads/test/ThreadSafeTest01.java
@@ -0,0 +1,29 @@
+package phquartin.maratonajava.javacore.ZZFthreads.test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class ThreadSafeNames { // Possui um problema mesmo com os atributos synchronized. Os metodos que acessam tambem precisam ser syncronized
+    private final List<String> names = Collections.synchronizedList(new ArrayList<String>());
+
+    public void add(String name) {
+        names.add(name);
+    }
+    public void removeFirst(){
+        if(!names.isEmpty()){
+            System.out.println(Thread.currentThread().getName());
+            System.out.println(names.removeFirst());
+        }
+    }
+}
+
+public class ThreadSafeTest01 {
+    public static void main(String[] args) {
+        ThreadSafeNames threadSafeNames = new ThreadSafeNames();
+        threadSafeNames.add("Julio");
+        Runnable r = threadSafeNames::removeFirst;
+        new Thread(r).start();
+        new Thread(r).start();
+    }
+}
