8556300fa1e993d51a528b53509d0dc583702206
phquartin <phquartinmatos@sempreceub.com>
2025-04-03 10:54:42 -0300

    feature: Aulas JBDC -> Lombok

--- src/main/java/phquartin/maratonajava/javacore/ZZIjbdc/test/ConnectionFactoryTest01.java
+++ src/main/java/phquartin/maratonajava/javacore/ZZIjbdc/test/ConnectionFactoryTest01.java
@@ -1,14 +1,13 @@
 package phquartin.maratonajava.javacore.ZZIjbdc.test;
 
-import phquartin.maratonajava.javacore.ZZIjbdc.conn.ConnectionFactory;
 import phquartin.maratonajava.javacore.ZZIjbdc.dominio.Producer;
 import phquartin.maratonajava.javacore.ZZIjbdc.repository.ProducerRepository;
 
 public class ConnectionFactoryTest01 {
     public static void main(String[] args) {
-        Producer nhk = Producer.ProducerBuilder.builder()
-                .name("NHK")
+        Producer producer = Producer.builder()
+                .name("Studio Deen")
                 .build();
-        ProducerRepository.save(nhk);
+        ProducerRepository.save(producer);
     }
 }
