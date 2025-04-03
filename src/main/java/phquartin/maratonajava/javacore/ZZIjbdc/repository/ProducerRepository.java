2b50ee48351dee2c971cc62957ba6d22559b2686
phquartin <phquartinmatos@sempreceub.com>
2025-04-03 11:07:56 -0300

    feature: Aulas JBDC -> log4j2

--- src/main/java/phquartin/maratonajava/javacore/ZZIjbdc/repository/ProducerRepository.java
+++ src/main/java/phquartin/maratonajava/javacore/ZZIjbdc/repository/ProducerRepository.java
@@ -1,5 +1,6 @@
 package phquartin.maratonajava.javacore.ZZIjbdc.repository;
 
+import lombok.extern.log4j.Log4j2;
 import phquartin.maratonajava.javacore.ZZIjbdc.conn.ConnectionFactory;
 import phquartin.maratonajava.javacore.ZZIjbdc.dominio.Producer;
 
@@ -7,6 +8,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+@Log4j2
 public class ProducerRepository {
     public static void save(Producer producer) {
         String sql = "INSERT INTO anime_store.producer (name) VALUES ('%s');".formatted(producer.getName());
@@ -15,7 +17,7 @@ public class ProducerRepository {
             Statement stmt = conn.createStatement())
         {
             int rowsAffected = stmt.executeUpdate(sql);
-            System.out.println("Rows affected: " + rowsAffected);
+            log.info("Inserted producer database, rows affected: '{}'", rowsAffected);
 
         } catch (SQLException e) {
             System.out.println(e.getMessage());
